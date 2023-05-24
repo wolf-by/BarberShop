817f18a1553f660ba18be34e5fc53b64a50ef952
wolf-by <wolf.by@yandex.ru>
2023-05-24 16:46:20 +0300

    db results_as_hash = true

--- app.rb
+++ app.rb
@@ -116,3 +116,13 @@ post '/contacts' do
 
 	erb "Данные отправлены" 
 end
+
+get '/showusers' do
+  "Hello World"
+end
+
+def get_db
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
+end
